b475ea8ffc9f380fec3bcc211977648669968b06
Jsgithubchannel <jsschannel99@gmail.com>
2022-07-30 14:28:43 +0900

    Feat(Router): Add pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "./pages/Auth";
+import SignUp from "./pages/SignUp";
+import Main from "./pages/Main";
 
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
