d344a3b190db07dd1af810a56593a3d75330fb67
Jsgithubchannel <jsschannel99@gmail.com>
2022-07-30 15:50:26 +0900

    Feat(SignUp): Enable submit btn conditionally

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Signup.module.scss";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [rePw, setRePw] = useState("");
+  const [isDisabled, setDisabled] = useState(true);
+
+  const changeInput = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPw(value);
+        break;
+      case "reEnterPassword":
+        setRePw(value);
+        break;
+      default:
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (pw !== rePw) {
+      alert("비밀번호가 일치하지 않습니다.");
+    } else {
+      alert("로그인 성공");
+      navigate("/auth");
+    }
+  };
+
+  useEffect(() => {
+    if (
+      email.includes("@") &&
+      email.includes(".") &&
+      pw.length >= 8 &&
+      rePw.length >= 8
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, pw.length, rePw.length]);
 
   return (
     <div className={styles.container}>
-      <form className={styles.column}>
+      <form className={styles.column} onSubmit={onSubmit}>
         <input
           name="email"
           type="email"
           autoComplete="new-email"
           placeholder="이메일"
+          onChange={changeInput}
           className="input-account"
           required
         />
@@ -21,6 +67,7 @@ const SignUp = () => {
           type="password"
           autoComplete="new-password"
           placeholder="비밀번호"
+          onChange={changeInput}
           className="input-account"
           required
         />
@@ -29,10 +76,17 @@ const SignUp = () => {
           type="password"
           autoComplete="new-password"
           placeholder="비밀번호 확인"
+          onChange={changeInput}
           className="input-account"
           required
         />
-        <button className={`btn-main ${styles.btn}`}>회원가입</button>
+        <button
+          type="submit"
+          disabled={isDisabled}
+          className={`btn-main ${styles.btn}`}
+        >
+          회원가입
+        </button>
       </form>
 
       <div className={styles.linkContainer}>
