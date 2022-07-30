a67714bb25c6de9a3bb5a1fc6c3822aef42a8845
Jsgithubchannel <jsschannel99@gmail.com>
2022-07-30 15:42:25 +0900

    Feat(Auth): Enable submit btn conditionally

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Auth.module.scss";
 
 const Auth = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [isDisabled, setDisabled] = useState(true);
+
+  const changeInput = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPw(value);
+        break;
+
+      default:
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    alert("로그인 성공");
+  };
+
+  useEffect(() => {
+    if (email.includes("@") && email.includes(".") && pw.length >= 8) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, pw.length]);
 
   return (
     <div className={styles.container}>
-      <form className={styles.column}>
+      <form className={styles.column} onSubmit={onSubmit}>
         <input
           name="email"
           type="email"
           autoComplete="current-email"
           placeholder="이메일"
+          onChange={changeInput}
           required
           className="input-account"
         />
@@ -21,10 +54,17 @@ const Auth = () => {
           type="password"
           autoComplete="current-password"
           placeholder="비밀번호"
+          onChange={changeInput}
           required
           className="input-account"
         />
-        <button className={`btn-main ${styles.loginBtn}`}>로그인</button>
+        <button
+          type="submit"
+          disabled={isDisabled}
+          className={`btn-main ${styles.loginBtn}`}
+        >
+          로그인
+        </button>
       </form>
 
       <div className={styles.linkContainer}>
