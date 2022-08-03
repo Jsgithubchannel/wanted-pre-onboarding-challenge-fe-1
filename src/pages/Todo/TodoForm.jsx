ac74f86255397bb36f05f568fd09657f636af182
Jsgithubchannel <jsschannel99@gmail.com>
2022-08-04 00:06:10 +0900

    Design(TodoForm): Add content form

--- src/pages/Todo/TodoForm.jsx
+++ src/pages/Todo/TodoForm.jsx
@@ -3,23 +3,34 @@ import styles from "./TodoForm.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 const TodoForm = () => {
-  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // value && addTask(value)
-    setValue("");
+    setTitle("");
+    setContent("");
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input
-        type="text"
-        value={value}
-        placeholder="할 일을 작성해보세요."
-        onChange={(e) => setValue(e.target.value)}
-        className="input-todo"
-      />
+      <div className={styles.wrapper}>
+        <input
+          type="text"
+          value={title}
+          placeholder="할 일을 작성해보세요."
+          onChange={(e) => setTitle(e.target.value)}
+          className="input-todo"
+        />
+        <input
+          type="text"
+          value={content}
+          placeholder="상세 내용을 작성해보세요."
+          onChange={(e) => setContent(e.target.value)}
+          className={`input-todo ${styles.paragraph}`}
+        />
+      </div>
       <button type="submit">
         <FontAwesomeIcon icon={faPlus} />
       </button>
