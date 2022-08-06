dd9ff628e3621ecb84efb07808efb5503f583906
Jsgithubchannel <jsschannel99@gmail.com>
2022-08-06 22:30:46 +0900

    Feat(TodoDetail): Show title, content from state

--- src/pages/TodoDetail/TodoDetail.jsx
+++ src/pages/TodoDetail/TodoDetail.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import styles from "./TodoDetail.module.scss";
 
 const TodoDetail = () => {
   const { state } = useLocation();
-  return <div>detail</div>;
+  return (
+    <div className={styles.container}>
+      <h1>{state.title}</h1>
+      <hr />
+      <p>{state.content}</p>
+    </div>
+  );
 };
 
 export default TodoDetail;
