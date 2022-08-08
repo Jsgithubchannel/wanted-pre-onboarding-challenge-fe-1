665313235d37bfd0dc0ce7f6c7264a8b0d80ef48
Jsgithubchannel <jsschannel99@gmail.com>
2022-08-09 00:22:52 +0900

    Fix(Route): Alert re-login

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const isLoggedIn = window.localStorage.getItem("token");
+  if (!isLoggedIn) alert("다시 로그인 해주세요.");
   return isLoggedIn ? <Outlet /> : <Navigate to="/auth" />;
 };
 export default PrivateRoute;
