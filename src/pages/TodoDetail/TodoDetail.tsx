5dad5f6cb20f5b9114f1fb91d03e250f6ae8289f
Jsgithubchannel <jsschannel99@gmail.com>
2022-08-16 14:22:11 +0900

    Refactor: Route, TodoDetail ts로 변환

--- src/pages/TodoDetail/TodoDetail.tsx
+++ src/pages/TodoDetail/TodoDetail.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./TodoDetail.module.scss";
 
-const TodoDetail = () => {
+interface LocationState {
+  state: {
+    [key: string]: string;
+  };
+}
+
+const TodoDetail: React.FC = () => {
   const navigate = useNavigate();
-  const { state } = useLocation();
+  const { state } = useLocation() as LocationState;
+
   return (
     <div className={styles.container}>
-      <h1>{state.title}</h1>
+      <h1>{state?.title}</h1>
       <hr />
-      <p>{state.content}</p>
+      <p>{state?.content}</p>
       <button className="btn-main" onClick={() => navigate("/")}>
         홈으로 가기
       </button>
--- src/routers/PublicRoute.tsx
+++ src/routers/PublicRoute.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom";
 
-const PublicRoute = ({ restricted }) => {
+const PublicRoute = ({ restricted }: any) => {
   const isLoggedIn = window.localStorage.getItem("token");
   return isLoggedIn && restricted ? <Navigate to="/" /> : <Outlet />;
 };
